ca2b09c63df6ad2fc0e2779acc9d0026dbd9359e
john <john.denny@gmail.com>
2025-01-11 18:19:46 +0000

    Skeletal version of swing GUI

---
+++ johnjava/src/main/java/quantum/QCSwing.java
@@ -0,0 +1,74 @@
+package quantum;
+
+import java.awt.BorderLayout;
+import java.awt.Container;
+import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JTextArea;
+import javax.swing.JTextField;
+import javax.swing.SwingUtilities;
+
+/**
+ * Simple java application to create a graphical user interface (GUI).
+ * The user types in a string, which is added to a display area
+ * in the middle of the window.
+ */
+public class QCSwing implements ActionListener {
+	private JTextField textField;
+	private JTextArea messageArea;
+
+	public static void main(String[] args) {
+		//Schedule a job for the event-dispatching thread:
+        //creating and showing this application's GUI.
+        SwingUtilities.invokeLater(new Runnable() {
+            public void run() {
+            	new QCSwing();
+            }
+        });
+	}
+	public QCSwing() {
+		JFrame frame = new JFrame("quantum.HelloSwing");
+		JLabel textLabel = new JLabel("Type And Go:");
+		textField = new JTextField(20);
+		//!! messageArea = new JTextArea(10, 20);
+		JButton goButton = new JButton("Go");
+		JButton clearButton = new JButton("Clear");
+		Container container = frame.getContentPane();
+		JPanel controlPanel = new JPanel();
+        JPanel fieldsPanel = new JPanel();
+        fieldsPanel.setLayout(new GridLayout(2, 2));
+        fieldsPanel.add(new JLabel("x"));
+        fieldsPanel.add(new JTextField(20));
+        fieldsPanel.add(new JLabel("y"));
+        fieldsPanel.add(new JTextField(20));
+		container.add(controlPanel, BorderLayout.NORTH);
+		//!! container.add(messageArea, BorderLayout.CENTER);
+        container.add(fieldsPanel, BorderLayout.CENTER);
+		controlPanel.add(textLabel);
+		controlPanel.add(textField);
+		controlPanel.add(goButton);
+		controlPanel.add(clearButton);
+		goButton.addActionListener(this); // action is click on button...
+		textField.addActionListener(this); // ... or press Enter
+		clearButton.addActionListener(this);
+		frame.pack();
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setVisible(true); // start event handling thread
+	}
+	@Override
+	public void actionPerformed(ActionEvent event) {
+		String action = event.getActionCommand();
+		if (action.equals("Clear")) {
+			textField.setText("");
+		} else {
+			messageArea.append(textField.getText() + "\n");
+		}
+	}
+}
+
