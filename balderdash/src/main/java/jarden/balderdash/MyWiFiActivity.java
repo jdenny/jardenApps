8e01b2e3091c9f94ae06188ab2d910d295e2bcae
john <john.denny@gmail.com>
2025-12-27 22:35:29 +0000

    New module Balderdash

---
+++ balderdash/src/main/java/jarden/balderdash/MyWiFiActivity.java
@@ -0,0 +1,7 @@
+package jarden.balderdash;
+
+/**
+ * Created by john.denny@gmail.com on 26/12/2025.
+ */
+public class MyWiFiActivity {
+}
---
+++ balderdash/src/main/java/jarden/balderdash/WiFiDirectBroadcastReceiver.java
@@ -0,0 +1,46 @@
+package jarden.balderdash;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.net.wifi.p2p.WifiP2pManager;
+import android.net.wifi.p2p.WifiP2pManager.Channel;
+import android.util.Log;
+
+/**
+ * Created by john.denny@gmail.com on 26/12/2025.
+ */
+public class WiFiDirectBroadcastReceiver extends BroadcastReceiver {
+    public static final String TAG = "WiFiDirectBroadcastReceiver";
+    private WifiP2pManager manager;
+    private Channel channel;
+    private MyWiFiActivity activity;
+
+    public WiFiDirectBroadcastReceiver(WifiP2pManager manager, Channel channel,
+                                       MyWiFiActivity activity) {
+        super();
+        this.manager = manager;
+        this.channel = channel;
+        this.activity = activity;
+    }
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        String action = intent.getAction();
+
+        if (WifiP2pManager.WIFI_P2P_STATE_CHANGED_ACTION.equals(action)) {
+            int state = intent.getIntExtra(WifiP2pManager.EXTRA_WIFI_STATE, -1);
+            if (state == WifiP2pManager.WIFI_P2P_STATE_ENABLED) {
+                /*if (BuildConfig.DEBUG)*/ Log.d(TAG, "WIFI_P2P_STATE_ENABLED");
+            } else {
+                /*if (BuildConfig.DEBUG)*/ Log.d(TAG, "NOT WIFI_P2P_STATE_ENABLED");
+            }
+        } else if (WifiP2pManager.WIFI_P2P_PEERS_CHANGED_ACTION.equals(action)) {
+            // Call WifiP2pManager.requestPeers() to get a list of current peers
+        } else if (WifiP2pManager.WIFI_P2P_CONNECTION_CHANGED_ACTION.equals(action)) {
+            // Respond to new connection or disconnections
+        } else if (WifiP2pManager.WIFI_P2P_THIS_DEVICE_CHANGED_ACTION.equals(action)) {
+            // Respond to this device's wifi state changing
+        }
+    }
+}
