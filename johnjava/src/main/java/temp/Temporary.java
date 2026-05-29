b2c5db2bbaf8e10ef461d8b66ed722d90dc9a934
john <john.denny@gmail.com>
2026-05-29 08:19:10 +0100

    Threads nearly implemented!

--- johnjava/src/main/java/temp/Temporary.java
+++ johnjava/src/main/java/temp/Temporary.java
@@ -4,10 +4,18 @@ package temp;
  * Created by john.denny@gmail.com on 06/03/2026.
  */
 public class Temporary {
+    private String name = "hello John";
     public static void main(String[] args) {
-        System.out.println("Cores: " +
-                Runtime.getRuntime().availableProcessors()
-        );
-
+        new Temporary();
+    }
+    public Temporary() {
+        new TestClass();
+    }
+    private class TestClass {
+        public TestClass() {
+            System.out.println(name);
+        }
     }
 }
+
+
--- life/src/main/java/evolvingwords/EvolveWords.java
+++ life/src/main/java/evolvingwords/EvolveWords.java
@@ -8,6 +8,8 @@ import java.util.HashSet;
 import java.util.List;
 import java.util.Random;
 import java.util.Set;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 /**
  * Created by john.denny@gmail.com on 16/05/2026.
@@ -29,7 +31,7 @@ Observations so far:
  */
 public class EvolveWords {
     private final static boolean debug = false;
-    // private final ExecutorService executor = Executors.newCachedThreadPool();
+    private final ExecutorService executor = Executors.newCachedThreadPool();
     private final static char[] letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".toCharArray();
     private final Set<String> wordSet = new HashSet<>();
     private static final ScrabWord adamScrab = new ScrabWord("WARM");
@@ -48,8 +50,8 @@ public class EvolveWords {
             wordSet.add(line);
         }
         System.out.println("words loaded: " + wordSet.size());
-        run();
-        // executor.shutdown();
+        //!! run();
+        executor.shutdown();
         printTree3(adamScrab, "");
     }
     public static void printTree3(ScrabWord scrabWord, String prefix) {
@@ -74,79 +76,92 @@ public class EvolveWords {
         }
     }
 
-    private void run() {
-        ScrabWord parentScrab = adamScrab;
-        String nextWord;
-        ScrabWord nextScrab;
-        for (int i = 0; i < 10; i++) {
-            try {
-                nextWord = findNextWord(parentScrab, 80);
-                nextScrab = new ScrabWord(nextWord);
-                parentScrab.addChild(nextScrab);
-                parentScrab = nextScrab;
-            } catch (NoSuitableWordFoundException e) {
-                System.out.println("no suitable word found");
-                return;
-            }
+    private class EvolveOneWord implements Runnable {
+        private final ScrabWord parentScrab;
+        public EvolveOneWord(ScrabWord parentScrab) {
+            this.parentScrab = parentScrab;
         }
-    }
-    private String findNextWord(ScrabWord parentScrab, int limit)
-            throws NoSuitableWordFoundException {
-        String nextWord;
-        String parentWord = parentScrab.getWord();
-        int parentValue = parentScrab.getValue();
-        for (int i = 1; i <= limit; i++) {
-            nextWord = mutate(parentWord);
-            if (isWord(nextWord)) {
-                System.out.println(nextWord + " found after " + i + " mutations");
-                int value = ScrabWord.calculateValue(nextWord);
-                if (value >= parentValue) {
-                    System.out.println(nextWord + " has value " + value +
-                            " >= parent value " + parentValue);
-                    return nextWord;
+
+        public void run() {
+            String nextWord;
+            ScrabWord nextScrab;
+            for (int i = 0; i < 10; i++) {
+                try {
+                    nextWord = findNextWord(parentScrab, 80);
+                    nextScrab = new ScrabWord(nextWord);
+                    parentScrab.addChild(nextScrab);
+                    EvolveOneWord child = new EvolveOneWord(nextScrab);
+                    executor.execute(child);
+                    //!! parentScrab = nextScrab;
+                } catch (NoSuitableWordFoundException e) {
+                    System.out.println("no suitable word found");
+                    return;
                 }
             }
         }
-        throw new NoSuitableWordFoundException();
-    }
-    private String mutate(String word) {
-        String mutated;
-        Random random = new Random();
-        int a = random.nextInt(3);
-        if (a == 0) { // add letter
-            int b = random.nextInt(word.length() + 1);
-            char c = getRandomLetter();
-            mutated = word.substring(0, b) + c + word.substring(b);
-            if (debug) System.out.println("added " + c + " to position " + b);
-        } else if (a == 1) { // remove letter
-            int b = random.nextInt(word.length());
-            mutated = word.substring(0, b) + word.substring(b + 1);
-            if (debug) System.out.println("removed letter at position " + b);
-        } else { // replace letter
-            int b = random.nextInt(word.length());
-            char c = getRandomLetter(word.charAt(b));
-            mutated = word.substring(0, b) + c + word.substring(b + 1);
-            if (debug) System.out.println("replaced letter at position " + b + " with " + c);
+
+        private String findNextWord(ScrabWord parentScrab, int limit)
+                throws NoSuitableWordFoundException {
+            String nextWord;
+            String parentWord = parentScrab.getWord();
+            int parentValue = parentScrab.getValue();
+            for (int i = 1; i <= limit; i++) {
+                nextWord = mutate(parentWord);
+                if (isWord(nextWord)) {
+                    System.out.println(nextWord + " found after " + i + " mutations");
+                    int value = ScrabWord.calculateValue(nextWord);
+                    if (value >= parentValue) {
+                        System.out.println(nextWord + " has value " + value +
+                                " >= parent value " + parentValue);
+                        return nextWord;
+                    }
+                }
+            }
+            throw new NoSuitableWordFoundException();
         }
-        if (debug) System.out.println("mutated word: " + mutated);
-        return mutated;
-    }
-    private char getRandomLetter() {
-        Random random = new Random();
-        int index = random.nextInt(letters.length);
-        return letters[index];
-    }
-    private char getRandomLetter(char except) {
-        Random random = new Random();
-        int index = random.nextInt(letters.length);
-        char c = letters[index];
-        if (c == except) {
-            if (++index >= letters.length) index = 0;
-            c = letters[index];
+
+        private String mutate(String word) {
+            String mutated;
+            Random random = new Random();
+            int a = random.nextInt(3);
+            if (a == 0) { // add letter
+                int b = random.nextInt(word.length() + 1);
+                char c = getRandomLetter();
+                mutated = word.substring(0, b) + c + word.substring(b);
+                if (debug) System.out.println("added " + c + " to position " + b);
+            } else if (a == 1) { // remove letter
+                int b = random.nextInt(word.length());
+                mutated = word.substring(0, b) + word.substring(b + 1);
+                if (debug) System.out.println("removed letter at position " + b);
+            } else { // replace letter
+                int b = random.nextInt(word.length());
+                char c = getRandomLetter(word.charAt(b));
+                mutated = word.substring(0, b) + c + word.substring(b + 1);
+                if (debug) System.out.println("replaced letter at position " + b + " with " + c);
+            }
+            if (debug) System.out.println("mutated word: " + mutated);
+            return mutated;
         }
-        return c;
-    }
-    private boolean isWord(String word) {
-        return wordSet.contains(word);
+
+        private char getRandomLetter() {
+            Random random = new Random();
+            int index = random.nextInt(letters.length);
+            return letters[index];
+        }
+
+        private char getRandomLetter(char except) {
+            Random random = new Random();
+            int index = random.nextInt(letters.length);
+            char c = letters[index];
+            if (c == except) {
+                if (++index >= letters.length) index = 0;
+                c = letters[index];
+            }
+            return c;
+        }
+
+        private boolean isWord(String word) {
+            return wordSet.contains(word);
+        }
     }
 }
